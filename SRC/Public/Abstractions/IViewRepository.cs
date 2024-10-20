81cfdb85871e1362a4c7401456ca51c3f58fe451
denes.solti <sodnaatx@gmail.com>
2024-10-20 19:20:44 +0200

    add some docs

--- SRC/Public/Abstractions/IViewRepository.cs
+++ SRC/Public/Abstractions/IViewRepository.cs
@@ -7,22 +7,31 @@ using System;
 
 namespace Solti.Utils.Eventing.Abstractions
 {
+    /// <summary>
+    /// Specifies the write operations against a view repository.
+    /// </summary>
     public interface IViewRepositoryWriter
     {
         /// <summary>
-        /// Persits the given <see cref="Event"/>
+        /// Persits the given state
         /// </summary>
         void Persist(ViewBase view, string eventId, object?[] args);
     }
 
+    /// <summary>
+    /// Specifies the read operations against a view repository.
+    /// </summary>
     public interface IViewRepositoryWriterReader<IView> where IView: class
     {
         /// <summary>
-        /// Materializes the given view.
+        /// Materializes the view belongs to the given <paramref name="flowId"/>.
         /// </summary>
         IDisposable Materialize(string flowId, out IView view);
     }
 
+    /// <summary>
+    /// Represents an abstract repository to store view instances.
+    /// </summary>
     public interface IViewRepository<IView> : IViewRepositoryWriter, IViewRepositoryWriterReader<IView> where IView: class
     {
     }
