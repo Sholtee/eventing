4b4ab69730681d1db5b5931e3573c61f9f588f6d
denes.solti <sodnaatx@gmail.com>
2024-10-20 12:02:52 +0200

    test ViewInterceptor

---
+++ TEST/ViewInterceptorTests.cs
@@ -0,0 +1,71 @@
+/********************************************************************************
+* ViewInterceptorTests.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using Moq;
+using NUnit.Framework;
+
+namespace Solti.Utils.Eventing.Tests
+{
+    using Abstractions;
+    using Internals;
+    using Proxy.Generators;
+
+    [TestFixture]
+    public class ViewInterceptorTests
+    {
+        public interface IView
+        {
+            void Annotated(int param);
+
+            void NotAnnotated(string param);
+        }
+
+        public class View: ViewBase, IView
+        {
+            [Event(Name = "some-event")]
+            public void Annotated(int param) {}
+
+            public void NotAnnotated(string param){}
+        }
+
+        [Test]
+        public void InterceptorShouldDispatchEvents()
+        {
+            Mock<IUntypedViewRepository> mockEventRepo = new(MockBehavior.Strict);
+
+            View impl = new()
+            {
+                FlowId = "id",
+                OwnerRepository = mockEventRepo.Object
+            };
+
+            mockEventRepo.Setup(r => r.Persist(impl, "some-event", new object[] { 1986 }));
+
+            IView view = ProxyGenerator<IView, ViewInterceptor<View, IView>>.Activate(Tuple.Create(impl));
+            view.Annotated(1986);
+
+            mockEventRepo.Verify(r => r.Persist(impl, "some-event", new object[] { 1986 }), Times.Once);
+        }
+
+        [Test]
+        public void InterceptorShouldIgnoreNotAnnotatedMethods()
+        {
+            Mock<IUntypedViewRepository> mockEventRepo = new(MockBehavior.Strict);
+
+            View impl = new()
+            {
+                FlowId = "id",
+                OwnerRepository = mockEventRepo.Object
+            };
+
+            IView view = ProxyGenerator<IView, ViewInterceptor<View, IView>>.Activate(Tuple.Create(impl));
+            view.NotAnnotated("cica");
+
+            mockEventRepo.Verify(r => r.Persist(It.IsAny<ViewBase>(), It.IsAny<string>(), It.IsAny<object[]>()), Times.Never);
+        }
+    }
+}
